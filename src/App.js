ec03762445fa32b8f05521836fd815d1a88b33ad
Fschatbot <s.himanshu.sultania@fountainheadschools.org>
2022-07-16 21:22:00 +0530

    Added a simple name and email

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import "./index.css";
+import { faker } from "@faker-js/faker";
+
+function ProfileCard() {
+	let gender = faker.name.gender(true);
+	let firstName = faker.name.firstName(gender);
+	let secondName = faker.name.findName(firstName, undefined).replace(firstName, "").trim();
+	let email = faker.internet.email(firstName, secondName);
+	return (
+		<div className="Card">
+			<div className="Avatar">
+				<img src={`https://i.pravatar.cc/300?u=${email}`} alt="avatar" />
+			</div>
+			{faker.name.prefix(gender)} {faker.name.findName(firstName, secondName, gender)}
+		</div>
+	);
+}
 
 function App() {
-	return <>Hello World</>;
+	return <>{<ProfileCard />}</>;
 }
 
 export default App;
