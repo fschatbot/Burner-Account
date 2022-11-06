a9f051145b83f8aa1361a6872fba84b3c5a4da3e
Fschatbot <s.himanshu.sultania@fountainheadschools.org>
2022-11-06 13:10:25 +0530

    Fixed few issues and console spam

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 
 function CopyText(text) {
-	console.log(text);
 	return () => {
 		navigator.clipboard.writeText(text);
 		toast.success("Copied to Clipboard");
@@ -30,7 +29,6 @@ function getPersonInfo() {
 	let cardCVV = faker.finance.creditCardCVV();
 	let cardExpiry = "Lorem" || faker.finance.creditCardExpiry();
 
-	console.log("New Personal Information Fetched");
 	return { gender, firstName, secondName, email, fullName, phoneNumber, SuggPass, cardIssuer, cardNumber, cardCVV, cardExpiry };
 }
 
@@ -117,8 +115,8 @@ function ProfileCard() {
 			</div>
 			<a signature="" href="https://github.com/fschatbot" target="_blank" rel="noreferrer">
 				Made by FSChatBot
-				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-					<path stroke-linecap="round" stroke-linejoin="round" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+					<path strokeLinecap="round" strokeLinejoin="round" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
 				</svg>
 			</a>
 		</div>
